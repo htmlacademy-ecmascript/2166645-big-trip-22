a520caf429343d90062b09c292887e7cecd34873
Инга Егонская <ivegon@mail.ru>
2023-12-21 00:54:55 +0700

    Создает моки: offers, points, destinations

---
+++ src/mocks/offers.js
@@ -0,0 +1,102 @@
+export const offersMocksArray = [
+  {
+    type: 'taxi',
+    offers: [
+      {
+        id: '1.1of',
+        title: 'Upgrade to a business class',
+        price: 120
+      },
+      {
+        id: '1.2of',
+        title: 'Order Uber',
+        price: 50
+      },
+      {
+        id: '1.3of',
+        title: 'Add luggage',
+        price: 20
+      }
+    ]
+  },
+  {
+    type: 'bus',
+    offers: [
+      {
+        id: '2.1of',
+        title: 'Choose a seat in front',
+        price: 50
+      },
+      {
+        id: '2.2of',
+        title: 'Add luggage',
+        price: 10
+      }
+    ]
+  },
+  {
+    type: 'ship',
+    offers: [
+      {
+        id: '3.1of',
+        title: 'Choose luxury caban',
+        price: 480
+      },
+      {
+        id: '3.2of',
+        title: 'Add another meal',
+        price: 150
+      },
+      {
+        id: '3.3of',
+        title: 'Upgrade to a business class',
+        price: 300
+      }
+    ]
+  },
+  {
+    type: 'train',
+    offers: [
+      {
+        id: '4.1of',
+        title: 'Upgrade to a business class',
+        price: 180
+      },
+      {
+        id: '4.2of',
+        title: 'Add animal',
+        price: 80
+      }
+    ]
+  },
+  {
+    type: 'flight',
+    offers: [
+      {
+        id: '5.1of',
+        title: 'Add luggage',
+        price: 30
+      },
+      {
+        id: '5.2of',
+        title: 'Switch to comfort class',
+        price: 100
+      },
+      {
+        id: '5.3of',
+        title: 'Add meal',
+        price: 15
+      },
+      {
+        id: '5.4of',
+        title: 'Choose seats',
+        price: 5
+      },
+      {
+        id: '5.5of',
+        title: 'Travel by train',
+        price: 40
+      }
+    ]
+  }
+]
---
+++ src/mocks/points.js
@@ -0,0 +1,76 @@
+export const pointsMocksArray = [
+  {
+    id: '1a',
+    basePrice: 1100,
+    dateFrom: '2019-07 - 10T22: 55: 56.845Z',
+    dateTo: '2019-07 - 11T11: 22: 13.375Z',
+    destination: '1d',
+    isFavorite: false,
+    offers: [
+      '1.1of',
+      '1.2of',
+      '1.3of'
+    ],
+    type: 'taxi'
+  },
+
+  {
+    id: '2a',
+    basePrice: 1500,
+    dateFrom: '2022-06 - 10T22: 55: 56.845Z',
+    dateTo: '2022-06 - 11T11: 22: 13.375Z',
+    destination: '5d',
+    isFavorite: true,
+    offers: [
+      '3.1of',
+      '3.2of',
+      '3.3of'
+    ],
+    type: 'ship'
+  },
+
+  {
+    id: '3a',
+    basePrice: 950,
+    dateFrom: '2020-02 - 10T22: 55: 56.845Z',
+    dateTo: '2020-02 - 11T11: 22: 13.375Z',
+    destination: '3d',
+    isFavorite: true,
+    offers: [
+      '2.1of',
+      '2.2of'
+    ],
+    type: 'bus'
+  },
+
+  {
+    id: '4a',
+    basePrice: 1800,
+    dateFrom: '2019-08 - 10T22: 55: 56.845Z',
+    dateTo: '2019-08 - 11T11: 22: 13.375Z',
+    destination: '4d',
+    isFavorite: false,
+    offers: [
+      '4.1of',
+      '4.2of'
+    ],
+    type: 'train'
+  },
+
+  {
+    id: '5a',
+    basePrice: 3500,
+    dateFrom: '2019-10 - 10T22: 55: 56.845Z',
+    dateTo: '2019-11 - 11T11: 22: 13.375Z',
+    destination: '2d',
+    isFavorite: false,
+    offers: [
+      '5.1of',
+      '5.2of',
+      '5.3of',
+      '5.4of',
+      '5.5of'
+    ],
+    type: 'flight'
+  }
+]
